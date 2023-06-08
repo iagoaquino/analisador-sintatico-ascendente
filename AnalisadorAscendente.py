cc0cde3fdb92c6e8e3c0b9e913a94ee31897e041
iaquino <iagodeaquino10@gmail.com>
2023-06-08 10:59:59 -0300

    analise ascendente completa

--- AnalisadorAscendente.py
+++ AnalisadorAscendente.py
@@ -1,7 +1,9 @@
 from tabulate import tabulate
 from classes.automato import Automato
 from classes.token import Token
-alfabeto = "ifthenab"
+from classes.arvore import *
+
+alfabeto = "ifthenab "
 class AnalisadorAscendente:
     def __init__(self,automato):
         self.tabelaNumero = []
@@ -9,6 +11,10 @@ class AnalisadorAscendente:
         self.tabelaTransicao = []
         self.automato = automato
         self.entrada = ""
+        self.posicao = 0
+        self.pilhaEstado = []
+        self.pilhaNo = []
+        self.pilhaAcao = []
         self.token = Token("NULL","NULL")
         for i in range(9):
             linha = []
@@ -27,10 +33,100 @@ class AnalisadorAscendente:
             for j in range(3):
                 linha.append("[][]")
             self.tabelaTransicao.append(linha)
-            
+
     def set_entrada(self,entrada):
         self.entrada = entrada
+
+    def set_token(self,token):
+        self.token = token
+
+    def buscar_transicao_coluna(self,entrada):
+        if entrada == "E":
+            return 0
+        elif entrada == "C":
+            return 1
+        elif entrada == "S":
+            return 2
+        return 404
     
+    def fazer_transicao(self,entrada):
+        linha = self.pilhaEstado[-1]
+        coluna = self.buscar_transicao_coluna(entrada)
+        self.pilhaEstado.append(self.tabelaTransicao[linha][coluna])
+        
+    def reduct1(self):
+        no_pai = No("S")
+        for i in range(4):
+            no_pai.filhos.append(self.pilhaNo[-1])
+            self.pilhaNo.pop()
+            self.pilhaEstado.pop()
+        
+        no_pai.filhos.reverse()
+        self.pilhaNo.append(no_pai)
+        self.fazer_transicao("S")
+    
+    def reduct2(self):
+        no_pai = No("S")
+        no_pai.filhos.append(self.pilhaNo[-1])
+        self.pilhaNo.pop()
+        self.pilhaEstado.pop()
+        self.pilhaNo.append(no_pai)
+        self.fazer_transicao("S")
+
+    def reduct3(self):
+        no_pai = No("E")
+        no_pai.filhos.append(self.pilhaNo[-1])
+        self.pilhaNo.pop()
+        self.pilhaEstado.pop()
+        self.pilhaNo.append(no_pai)
+        self.fazer_transicao("E")
+    
+    def reduct4(self):
+        no_pai = No("C")
+        no_pai.filhos.append(self.pilhaNo[-1])
+        self.pilhaNo.pop()
+        self.pilhaEstado.pop()
+        self.pilhaNo.append(no_pai)
+        self.fazer_transicao("C")
+
+    def formar_token(self,estado_atual,valor_token):
+        token = Token("NULL","NULL")
+        if estado_atual == 2:
+            token.tipo = "if"
+            token.valor = "if"
+        if estado_atual == 6:
+            token.tipo = "then"
+            token.valor = "then"
+        if estado_atual == 7:
+            token.tipo = "a"
+            token.valor = "a"
+        if estado_atual == 8:
+            token.tipo = "b"
+            token.valor = "b"
+        return token
+    
+    def get_next_token(self):
+        if self.posicao == len(self.entrada):
+            token = Token("$","$")
+            self.set_token(token)
+        else:
+            atual_position = self.posicao
+            estado_atual = 0
+            valor_token = ""
+            while atual_position != len(self.entrada):
+                validador = 0
+                if self.entrada[atual_position] != " ":
+                    valor_token = valor_token+self.entrada[atual_position]
+                estado_atual,validador = self.automato.fazer_transicao(estado_atual,self.entrada[atual_position])
+                if self.automato.checar_aceitacao(estado_atual) == 1:
+                    self.set_token(self.formar_token(estado_atual,valor_token))
+                    estado_atual = 0
+                    self.posicao = atual_position+1
+                    break
+                if validador == 0:
+                    return 0
+                atual_position += 1
+
     def formarTabelas(self):
         self.tabelaAcao[0][0] = "shift"
         self.tabelaAcao[0][3] = "shift"
@@ -60,14 +156,68 @@ class AnalisadorAscendente:
         print(tabulate(self.tabelaNumero))
         print(tabulate(self.tabelaTransicao))
 
+    def buscarSimbolo(self,texto):
+        if texto == "if":
+            return 0
+        elif texto == "then":
+            return 1
+        elif texto == "a":
+            return 2
+        elif texto == "b":
+            return 3
+        elif texto == "$":
+            return 4 
+        return 404
+    
+    def fazerAnalise(self):
+        self.pilhaEstado.append(0)
+        self.get_next_token()
+        while 1:
+            row_table = []
+            linha = self.pilhaEstado[-1]
+            coluna = self.buscarSimbolo(self.token.valor)
+            acao = self.tabelaAcao[linha][coluna]
+            numero = self.tabelaNumero[linha][coluna]
+            row_table.append(self.pilhaEstado)
+            row_table.append(self.token.valor+self.entrada[self.posicao:len(self.entrada)])
+            row_table.append(acao+str(numero))
+            nos = []
+            for no in self.pilhaNo: 
+                nos.append(no.valor)
+            row_table.append(nos)
+            print(tabulate(row_table))
+            if acao == "aceite":
+                break
+            if acao == "shift":
+                self.pilhaEstado.append(numero)
+                no = No(self.token.valor)
+                self.pilhaNo.append(no)
+                self.get_next_token()
+            if acao == "reduct":
+                if numero == 1:
+                    self.reduct1()
+                elif numero == 2:
+                    self.reduct2()
+                elif numero == 3:
+                    self.reduct3()
+                elif numero == 4:
+                    self.reduct4()
+
+            
+            
+        print(self.token.valor)
+
+
 def criarAutomato(quantidade_estados,alfabeto):
     automato = Automato(alfabeto,quantidade_estados)
-    # criando a transição do token 'if'
+    #tratamento do " "
+    automato.adicionar_transicao(0," ",0)
 
+    # criando a transição do token 'if'
     automato.adicionar_transicao(0,"i",1)
     automato.adicionar_transicao(1,"f",2)
-    # criando a transição do token 'then'
 
+    # criando a transição do token 'then'
     automato.adicionar_transicao(0,"t",3)
     automato.adicionar_transicao(3,"h",4)
     automato.adicionar_transicao(4,"e",5)
@@ -89,3 +239,5 @@ def criarAutomato(quantidade_estados,alfabeto):
 # a função retorna criar_automato um automato que é passada para o construtor da classe
 analisador1 = AnalisadorAscendente(criarAutomato(9,alfabeto))
 analisador1.formarTabelas()
+analisador1.set_entrada("if a then b")
+analisador1.fazerAnalise()
--- classes/automato.py
+++ classes/automato.py
@@ -27,7 +27,7 @@ class Automato:
             transicao.append(estado_transicao)
             self.estados[estado_atual].transicoes.append(transicao)
         else:
-            print("alfabeto não aceito normal")
+            print("erro não foi possivel computar") 
 
     def adicionar_transicao_estrela(self,estado_atual,alfabeto,estado_transicao):
         if alfabeto in self.alfabeto and estado_transicao in range(len(self.estados)):
@@ -36,35 +36,53 @@ class Automato:
             transicao.append(estado_transicao)
             self.estados[estado_atual].transicoes_estrela.append(transicao)
         else:
-            print("alfabeto não aceito estrela") 
+            print("erro não foi possivel computar") 
 
     def definir_aceitacao(self,num_estado):
         self.estados_aceitacao.append(num_estado)
 
-    def finalizou_aceitou(self,estado_atual):
+    def checar_aceitacao(self,estado_atual):
         if estado_atual in self.estados_aceitacao:
             return 1
         return 0
 
     def fazer_transicao(self,estado_atual,alfabeto):
         novo_estado = estado_atual
+        fez_transicao = 0
         for i in range(len(self.estados[estado_atual].transicoes)):
             if alfabeto == self.estados[estado_atual].transicoes[i][0]:
                 novo_estado = self.estados[estado_atual].transicoes[i][1]
-        return novo_estado
+                fez_transicao = 1
+        if fez_transicao == 1:
+            return novo_estado,1
+        else:
+            return novo_estado,0
     
     def fazer_transicao_estrela(self,estado_atual,letra):
+        novo_estado = estado_atual
         for i in range(len(self.estados[estado_atual].transicoes_estrela)):
             #print(letra+", transicoes estrela"+str(self.estados[estado_atual].transicoes_estrela[i][0]))
             if letra == self.estados[estado_atual].transicoes_estrela[i][0]:
-                return 1
-        return 0
+                novo_estado = self.estados[estado_atual].transicoes_estrela[i][1]
+                return novo_estado,1
+        return novo_estado,0
             
     def mostrar_alfabeto(self):
         print(self.alfabeto)
 
     def mostrar_tamanho_estados(self):
         print(len(self.estados))
+
+    def mostrar_transicoes(self):
+        for pos in range(len(self.estados)):
+            print("estado: "+str(pos))
+            self.estados[pos].mostrar_transicoes()
+
+    def mostrar_transicoes_estrela(self):
+        for pos in range(len(self.estados)):
+            print("estado: "+str(pos))
+            self.estados[pos].mostrar_transicoes_estrela()
+                    
     def mostrar_aceitacao(self):
         print(self.estados_aceitacao)
 
